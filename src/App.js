829b182c77b508ad1b0216126c9906cec9ffca08
luca-scocchi <lscocchi@gmail.com>
2023-07-05 09:24:48 +0200

    aggiornamento progetto react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import Componente1 from "./Componente1";
-import Componente2 from "./Componente2";
-import  MyComponent from "./MyComponent"
+import Component1 from "./Component1";
+import Component2 from "./Component2";
+
 import React, {useState, useEffect} from "react";
+
 
 
 const App = () => {
   const [ articles, setArticles] = useState([])
-  const [term, setTerm] = useState('everything')
-  const [isLoading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 const selectedImageIndex = 2;
 
   useEffect(() => {
+    setLoading(true);
     const fetchArticles = async () => {
     try {
-        const res = await fetch ('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=UwUope7u9WCLG7Ab2awwNEfWviZnNEdt')
+        const res = await fetch ('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=UwUope7u9WCLG7Ab2awwNEfWviZnNEdt');
         const articles = await res.json()
         console.log(articles.results);
         setArticles(articles.results)
+        setLoading(false);
     } catch (error){
       console.log(error);
       
@@ -27,55 +29,54 @@ const selectedImageIndex = 2;
   }, [])
 
   return (
-   <>
-   <div className="header">
-    <Componente1 className='giorno'></Componente1>
-    <div className="overlay">
-      <h1 className="titolo">   The Time Flies</h1>
-    </div>
-   </div>
-   <section>
-   {articles.map((article) => {
-    const {abstract, title, section, multimedia, byline, subsection, url, _id, } = article
+    <>
+      <div className="header">
+        <Component1 className="date" />
+        <div className="overlay">
+          <h1 className="title">The Time Flies</h1>
+        </div>
+      </div>
+      <section>
+        {loading ? (
+          <div className="loader"></div>
+        ) : (
+          articles.map((article) => {
+            const { abstract, title, section, multimedia, byline, subsection, url, _id } = article;
 
-    return (
-      <article className="article" key={_id}>
-<div className="titolino"><p>{section}</p></div>
-<h2>{title}</h2>
-<p>{abstract}</p>
+            return (
+              <article className="article" key={_id}>
+                <div className="subtitle">
+                  <p>{section}</p>
+                </div>
+                <h2>{title}</h2>
+                <p>{abstract}</p>
 
+                <div className="info">
+                  <p>{byline}</p>
+                  <p>{subsection}</p>
+                </div>
 
-<div className="coseinutili">
-<p>{byline}</p>
-<p>{subsection}</p>
+                <a href={url} class="button-link" rel="noopener noreferrer"> Web Resource</a>
+                
+              {multimedia && (
+              <div>
+              <div className="image-container">
+             {multimedia.map((image, index) => (
+              index === selectedImageIndex && (
+             <img key={image.id} src={image.url} alt={image.title} />)
+            ))}
+            </div>
+             </div>
+                  )}
+              </article>
+            );
+          })
+        )}
 
-</div>
- < a href= {url} target= "_blank"> Web Resource</a>
-{multimedia && (
-<div>
-   
-   <div className="image-container">
-   {multimedia.map((image, index) => (
-     index === selectedImageIndex && (
-       <img key={image.id} src={image.url} alt={image.title} />
-       )
-       ))}
-     </div>
-   </div>
- )}
-  
-
-
-      </article>
-    )
-   })}
-  <Componente2/>
-   </section>
-   
-   </>
+        <Component2 />
+      </section>
+    </>
   );
+};
 
-  
-}
 export default App;
-
---
+++ src/Component2.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+function Component2() {
+  return (
+    <div className='footer'>
+      <p className='smallText'>Copyright:2022 Luca Scocchi</p>
+    </div>
+  );
+  }
+
+export default Component2;
